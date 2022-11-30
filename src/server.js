dfdcfb9f6b76775e0293daa620defca62b3bb80a
mamush-m <svrmamush@gmail.com>
2022-11-30 05:14:47 -0500

    messages sent to server successfully

--- src/server.js
+++ src/server.js
@@ -13,11 +13,18 @@ app.get('/', (req, res) => {
     res.sendFile(file)
 });
 
+
 io.on('connection', socket => {
     console.log('a user connected');
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
+    });
+});
+
+io.on('connection', socket => {
+    socket.on('chat message', msg => {
+      console.log('message: ' + msg);
     });
 });
 
