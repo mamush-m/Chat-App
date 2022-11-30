ff53e569c91605437244db8b068a1a9b77ad1cc1
mamush-m <svrmamush@gmail.com>
2022-11-30 05:24:06 -0500

    get started section done

--- src/server.js
+++ src/server.js
@@ -24,7 +24,8 @@ io.on('connection', socket => {
 
 io.on('connection', socket => {
     socket.on('chat message', msg => {
-      console.log('message: ' + msg);
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
     });
 });
 
